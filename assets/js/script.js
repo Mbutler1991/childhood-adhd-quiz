8ca3343c905fc30fcaa478377da14e408d8fa114
Mbutler1991 <markbutlermb1991@gmail.com>
2023-11-26 01:14:53 +0000

    added functionality for splitting results and for making results show.

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,37 +30,26 @@ let questions = ['Sassy to grown-ups.',
 
 //array for storing answers and different metrics for scoring
 let answerArray = [];
-let adhdScore = 0;
-let conductScore = 0;
-let learningScore = 0;
-let psychosomaticScore = 0;
-let hyperScore = 0;
-let anxietyScore = 0;
+let adhdScore;
+let conductScore;
+let learningScore;
+let psychosomaticScore;
+let hyperScore;
+let anxietyScore;
 //index number 
 i = 0;
 
-document.getElementById('submit').style.visibility = 'collapse';
-document.getElementById('previous').style.visibility = 'collapse';
+document.getElementById('submit').style.display = 'none';
+document.getElementById('previous').style.display = 'none';
 
 //make all scores hidden
-if (conductScore < 14) {
-    document.getElementById('adhd-result').style.visibility = 'collapse';
-}
-if (conductScore < 14) {
-    document.getElementById('conduct-result').style.visibility = 'collapse';
-}
-if (learningScore < 7) {
-    document.getElementById('learning-result').style.visibility = 'collapse';
-}
-if (psychosomaticScore < 7) {
-    document.getElementById('psychosomatic-result').style.visibility = 'collapse';
-}
-if (hyperScore < 7) {
-    document.getElementById('hyper-result').style.visibility = 'collapse';
-}
-if (conductScore < 14) {
-    document.getElementById('anxiety-result').style.visibility = 'collapse';
-}
+
+document.getElementById('adhd-result').style.display = 'none';
+document.getElementById('conduct-result').style.display = 'none'; 
+document.getElementById('learning-result').style.display = 'none';
+document.getElementById('psychosomatic-result').style.display = 'none';
+document.getElementById('hyper-result').style.display = 'none'; 
+document.getElementById('anxiety-result').style.display = 'none';
 
 //function that displays current question and next question
 function displayQuestions() {
@@ -76,9 +65,8 @@ function displayQuestions() {
             <option value='3'>Very Much</option>
         </select>`  
         document.getElementById('start').innerText = 'Next Question!';
-        document.getElementById('previous').style.visibility = 'visible';
+        document.getElementById('previous').style.display = 'inline';
         i++;  
-        
         
         let answer = document.getElementById('each-question');
         let btn = document.getElementById('submit');
@@ -87,11 +75,12 @@ function displayQuestions() {
         gatherScore = answer.value;
         //console.log(gatherScore);
         answerArray.push(gatherScore);
+        document.getElementById('reveal').style.display = 'inline';
         //console.log(answerArray); 
         calculateScores();
         })     
     } else {
-        document.getElementById('submit').style.visibility = 'visible';
+        document.getElementById('submit').style.display = 'inline';
     }
 }  
 
@@ -115,38 +104,51 @@ function previousQuestion() {
 function calculateScores() {
     console.log(answerArray);
     adhdScore = parseInt(answerArray[1]) + parseInt(answerArray[4]) + parseInt(answerArray[5]) + parseInt(answerArray[7]) + parseInt(answerArray[10]) + parseInt(answerArray[12]) + parseInt(answerArray[14]) + parseInt(answerArray[15]) + parseInt(answerArray[17]) + parseInt(answerArray[19]);
+    document.getElementById('a-score').innerHTML = `${adhdScore}`;
     conductScore = parseInt(answerArray[0]) + parseInt(answerArray[3]) + parseInt(answerArray[8]) + parseInt(answerArray[10]) + parseInt(answerArray[11]) + parseInt(answerArray[13]) + parseInt(answerArray[18]) + parseInt(answerArray[20]);
+    document.getElementById('b-score').innerHTML = `${conductScore}`;
     learningScore = parseInt(answerArray[4]) + parseInt(answerArray[12]) + parseInt(answerArray[15]) + parseInt(answerArray[19]);
+    document.getElementById('c-score').innerHTML = `${learningScore}`;
     psychosomaticScore = parseInt(answerArray[16]) + parseInt(answerArray[21]) + parseInt(answerArray[22]) + parseInt(answerArray[23]);
+    document.getElementById('d-score').innerHTML = `${psychosomaticScore}`;
     hyperScore = parseInt(answerArray[1]) + parseInt(answerArray[2]) + parseInt(answerArray[5]) + parseInt(answerArray[7]);
+    document.getElementById('e-score').innerHTML = `${hyperScore}`;
     anxietyScore = parseInt(answerArray[6]) + parseInt(answerArray[9]);
-
-    displayResult();
+    document.getElementById('f-score').innerHTML = `${anxietyScore}`;
+    
 }
 
 function displayResult() {
-    document.getElementById('submit').style.visibility = 'collapse';
-    document.getElementsByClassName('quiz').style.visibility = 'collapse';
-    document.getElementById('quiz').style.visibility = 'collapse';
-    document.getElementById('previous').style.visibility = 'collapse';
-    document.getElementById('start').style.visibility = 'collapse';
+    document.getElementById('submit').style.display = 'none';
+   // document.getElementsByClassName('quiz').style.display = 'none';
+   // document.getElementById('quiz').style.display = 'none';
+    document.getElementById('previous').style.display = 'none';
+    document.getElementById('start').style.display = 'none';  
 
+    adhdScore = parseInt(document.getElementById('a-score').innerHTML);
+    conductScore = parseInt(document.getElementById('b-score').innerHTML);
+    learningScore = parseInt(document.getElementById('c-score').innerHTML);
+    psychosomaticScore = parseInt(document.getElementById('d-score').innerHTML);
+    hyperScore = parseInt(document.getElementById('e-score').innerHTML);
+    anxietyScore = parseInt(document.getElementById('f-score').innerHTML);
+    
     if (adhdScore >= 18) {
-        document.getElementById('adhd-result').style.visibility = 'visible';
+        document.getElementById('adhd-result').style.display = 'block';
     } 
+    
     if (conductScore >= 14) {
-        document.getElementById('conduct-result').style.visibility = 'visible';
-    }
+        document.getElementById('conduct-result').style.display = 'block';
+    } 
     if (learningScore >= 7) {
-        document.getElementById('learning-result').style.visibility = 'visible';
+        document.getElementById('learning-result').style.display = 'block';
     }
     if (psychosomaticScore >= 7) {
-        document.getElementById('psychosomatic-result').style.visibility = 'visible';
-    }
+        document.getElementById('psychosomatic-result').style.display = 'block';
+    } 
     if (hyperScore >= 7) {
-        document.getElementById('hyper-result').style.visibility = 'visible';
-    }
+        document.getElementById('hyper-result').style.display = 'block';
+    } 
     if (anxietyScore >= 3) {
-        document.getElementById('anxiety-result').style.visibility = 'visible';
-    }
+        document.getElementById('anxiety-result').style.display = 'block';
+    } 
 }   
